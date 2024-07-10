658018bdd8d129c9858a01ca4db057004768d4be
Akash <akash.dhembare777@gmail.com>
2024-07-10 12:22:41 +0530

    Commit

---
+++ src/ex_07072024/Lab003.java
@@ -0,0 +1,14 @@
+package ex_07072024;
+
+public class Lab003 {
+    public static void main(String[] args) {
+        //Single Line comment - this will not execute
+
+        /* Multi-line comment
+        *
+        *
+        *
+        *
+         */
+    }
+}
---
+++ src/ex_07072024/Lab006.java
@@ -0,0 +1,8 @@
+package ex_07072024;
+
+public class Lab006 {
+    public static void main(String[] args) {
+        System.out.print("Lab005");
+        System.out.print("Lab006");
+    }
+}
